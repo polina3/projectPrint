ff37526e29f0415313ddd86c9bb1066c8eb6a786
Polina <root@mtegnbbebd.local>
2024-07-14 10:21:15 +0000

    поправила шапку

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import styles from "./layout.module.scss";
-import First from "./first/page";
+import './globals.scss'
 import Script from 'next/script'
+import Link from "next/link"; 
+import {montserrat} from './constant/fonts'
+
 
 export const metadata: Metadata = {
   title: "INSTANT FORGE PRO"
@@ -14,26 +17,28 @@ type TypeProps = {
 export default function RootLayout(props: TypeProps) {
   return (
     <html lang="en">
-      <body className={styles.all}>
-        <div className={styles.header_box}> 
+      <body className={`${styles.all} ${montserrat.variable}` }>
+        <div className={styles.header_box}>
           <header className={styles.header}>
-                <ul className={styles.ul}>
-                  <li className={styles.li}><a className={styles.a} href="/first">Главная</a></li>
-                  <li className={styles.li}><a className={styles.a} href="/about">О нас</a></li>
-                  <li className={styles.li}><a className={styles.a} href="/where">Где забрать</a></li>
-                  <li className={styles.li}><a className={styles.a} href="/services">Услуги</a></li>
-                  <li className={styles.li}><a className={styles.a} href="/calculator">Калькулятор</a></li>
-                </ul>
-                <div className={styles.rightSide} >
-                  <div className={styles.language}>
-                    <span>RU</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                      <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-                    </svg>
-                  </div>
-                  <button className={styles.btnBuy}>Заказать</button>
-                  <button className={styles.btnLogin}>Вход</button>
-                </div>
+            <nav>
+              <ul className={styles.ul}>
+                <li><Link className={styles.nav_link} href="/first">Главная</Link></li>
+                <li><Link className={styles.nav_link} href="/about">О нас</Link></li>
+                <li><Link className={styles.nav_link} href="/where">Где забрать</Link></li>
+                <li><Link className={styles.nav_link} href="/services">Услуги</Link></li>
+                <li><Link className={styles.nav_link} href="/calculator">Калькулятор</Link></li>
+              </ul>
+            </nav>
+            <div className={styles.rightSide} >
+              <div className={styles.language}>
+                <span>RU</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                </svg>
+              </div>
+              <button className={styles.btnBuy}>Заказать</button>
+              <button className={styles.btnLogin}>Вход</button>
+            </div>
           </header>
         </div>
         <main>
